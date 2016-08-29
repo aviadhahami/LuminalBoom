0327e8448c1c96b2f3dd0e75f78bca614da43cc9
Aviad Hahami <aviadhahami@gmail.com>
2016-08-29 08:05:05 +0300

    Fixes

---
+++ src/components/DemoAlert/DemoAlert.js
@@ -0,0 +1,48 @@
+/**
+ * Created by aviad on 8/29/2016.
+ */
+import './DemoAlert.css'
+import './../Home.css'
+import React,{Component} from 'react'
+import {Grid, Row, Col, FormControl, Button} from 'react-bootstrap'
+
+class DemoAlert extends Component{
+	
+	constructor(props){
+		super(props);
+	}
+	
+	render(){
+		return (
+			<div className="full-width full-height">
+				<Grid>
+					<Row className="show-grid graffiti-bg">
+						<Col md={12} className="logo-container">
+							<div className="lominal-circle-error">
+								<p className="lominal-text-error">
+									Oopsy!
+								</p>
+								<p className="lominal-text-error-subtitle">
+									This is just a prototype.
+								</p>
+							</div>
+						</Col>
+					</Row>
+					<Row className="show-grid width-100">
+						<Col md={12} className="signup-container">
+							<div className="signup-title">
+								<p>If you want to hear more from us in the future, please enter your email here:</p>
+							</div>
+							<div className="input-field">
+								<input className='input' type="text" placeholder="Enter your email address"/>
+								<a href="#" className="send-button">Send</a>
+							</div>
+						</Col>
+					</Row>
+				</Grid>
+			</div>
+		)
+	}
+}
+
+export default DemoAlert
--- src/routes.js
+++ src/routes.js
@@ -2,13 +2,14 @@ import React from 'react'
 import { Route } from 'react-router'
 import App from './components/App'
 import Home from './components/Home'
-import NotFound from './components/NotFound'
+import DemoAlert from './components/DemoAlert/DemoAlert'
 
 const routes = (
   <Route component={App}>
     <Route path='/' component={Home} />
+    <Route path='/DemoAlert' component={DemoAlert} />
     // Put other routes here
-    <Route path='*' component={NotFound} />
+    <Route path='*' component={Home} />
   </Route>
 )
 
