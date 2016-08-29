b848e45e1b9da4e133232e5b2e1fad871442cf15
Aviad Hahami <aviadhahami@gmail.com>
2016-08-29 17:22:28 +0300

    Starting

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -5,15 +5,46 @@
 import './About.css'
 import React, {Component} from 'react';
 
+import {Nav, NavItem, Grid, Row} from 'react-bootstrap'
 
 class About extends Component{
 	constructor(props){
 		super(props);
+		this.state={
+			activeKey : 1
+		}
+	}
+	
+	_handleSelect(selected_key){
+		this.setState({activeKey:selected_key});
 	}
 	
 	render(){
+		let currentView = <h2>Test</h2>;
+		switch (this.state.activeKey){
+			case 1:
+				currentView = <h2>1</h2>
+				break;
+			case 2:
+				currentView = <h2>2</h2>
+				break;
+			case 3:
+				currentView = <h2>3</h2>
+				break;
+		}
 		return (
-			<h2>About</h2>
+			<div>
+				<Nav bsStyle="tabs" className="flex-justify" activeKey={this.state.activeKey} onSelect={this._handleSelect.bind(this)}>
+					<NavItem eventKey={1} href="#">Hello</NavItem>
+					<NavItem eventKey={2} href="#">Team</NavItem>
+					<NavItem eventKey={3} href="#">Venture Proposal</NavItem>
+				</Nav>
+				<Grid>
+					<Row>
+						{currentView}
+					</Row>
+				</Grid>
+			</div>
 		)
 	}
 }
